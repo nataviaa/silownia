1cd4493a994325e9b894a56670ae58350acb25ca
nataviaa <65811938+nataviaa@users.noreply.github.com>
2021-01-09 20:15:17 +0100

    Update Wizyta_u_dietetyla.cpp

--- Wizyta_u_dietetyla.cpp
+++ Wizyta_u_dietetyla.cpp
@@ -7,5 +7,17 @@ using namespace std;
 #include "Dietetyk.h"
 #include "Klient.h"
 
+Wizyta_u_dietetyka::Wizyta_u_dietetyka(int a, double b, int c, Dietetyk* dietetyk, Klient* klient) {
+	_dzien = a;
+	_godzina_wizyty = b;
+	_cena = c;
+	_unnamed_Dietetyk_ = dietetyk;
+	_unnamed_Klient_ = klient;
+}
 
-
+int Wizyta_u_dietetyka::get_dzien() {
+	return _dzien;
+}
+double Wizyta_u_dietetyka::get_godzina() {
+	return _godzina_wizyty;
+}
