8f20ba66129943be8ec2e1f08b5c6e75a495572d
Klaudia19 <77021636+Klaudia19@users.noreply.github.com>
2021-01-12 01:06:39 +0100

    Update Karnet_miesieczny.cpp

--- Karnet_miesieczny.cpp
+++ Karnet_miesieczny.cpp
@@ -4,11 +4,13 @@ using namespace std;
 #include "Karnet_miesieczny.h"
 #include "Karnet.h"
 
-void Karnet_miesieczny::przedluz() {
-	throw "Not yet implemented";
+void Karnet_miesieczny::przedluz(Karnet*k) 
+{
+	
 }
 
-void Karnet_miesiêczny::zawies() {
-	throw "Not yet implemented";
+void Karnet_miesiêczny::zawies(Karnet* k1) 
+{
+	
 }
 
