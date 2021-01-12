bc2329b2346008cacdf0f56a593dfcee1f4f968e
Klaudia19 <77021636+Klaudia19@users.noreply.github.com>
2021-01-12 01:07:24 +0100

    Update Karnet_miesieczny.cpp

--- Karnet_miesieczny.cpp
+++ Karnet_miesieczny.cpp
@@ -4,12 +4,12 @@ using namespace std;
 #include "Karnet_miesieczny.h"
 #include "Karnet.h"
 
-void Karnet_miesieczny::przedluz(Karnet*k) 
+void Karnet_miesięczny::przedluz(Karnet*k) 
 {
 	
 }
 
-void Karnet_miesiêczny::zawies(Karnet* k1) 
+void Karnet_miesięczny::zawies(Karnet* k1) 
 {
 	
 }
