9891b9c2021e634979d5ee4efba1bcba12b8ab50
Klaudia19 <77021636+Klaudia19@users.noreply.github.com>
2021-01-12 18:49:54 +0100

    Update Wejście_jednorazowe.h

--- Wejście_jednorazowe.h
+++ Wejście_jednorazowe.h
@@ -1,15 +1,15 @@
 #include <exception>
 using namespace std;
 
-#ifndef __Wejœcie_jednorazowe_h__
-#define __Wejœcie_jednorazowe_h__
+#ifndef __Wejscie_jednorazowe_h__
+#define __Wejscie_jednorazowe_h__
 
 #include "Karnet.h"
 
 // class Karnet;
-class Wejœcie_jednorazowe;
+class Wejscie_jednorazowe;
 
-class Wejœcie_jednorazowe: public Karnet
+class Wejscie_jednorazowe: public Karnet
 {
 
 	public: void sprawdz_waznosc();
