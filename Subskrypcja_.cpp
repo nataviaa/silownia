1ed7884ec04972b6e15544f6ba3e29390d06a35b
Klaudia19 <77021636+Klaudia19@users.noreply.github.com>
2021-01-12 18:47:05 +0100

    Update Subskrypcja_.cpp

--- Subskrypcja_.cpp
+++ Subskrypcja_.cpp
@@ -4,11 +4,12 @@ using namespace std;
 #include "Subskrypcja_.h"
 #include "Karnet.h"
 
-void Subskrypcja_::przed³oz() {
-	throw "Not yet implemented";
+void Subskrypcja_::przedloz()
+{
+	
 }
 
-void Subskrypcja_::zawies() {
-	throw "Not yet implemented";
+void Subskrypcja_::zawies() 
+{
+	
 }
-
