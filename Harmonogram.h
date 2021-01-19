74bdae9f06ad5af1f375a788f2c3ed63df933685
Ejdzrian <60348246+Ejdzrian@users.noreply.github.com>
2021-01-19 19:39:02 +0100

    Add files via upload

---
+++ Harmonogram.h
@@ -0,0 +1,41 @@
+#include <exception>
+#include <vector>
+using namespace std;
+
+#ifndef __Harmonogram_h__
+#define __Harmonogram_h__
+
+#include "Karnet.h"
+//#include "Pracownik.h"
+#include "Klient.h"
+#include "Grupa_zajeciowa.h"
+
+class Harmonogram;
+class Grupa_zajeciowa;
+
+
+class Harmonogram
+{
+public:
+	string _miesiac;
+	Trener _trener;
+	Grupa_zajeciowa poniedzialek[5];
+	Grupa_zajeciowa wtorek[5];
+	Grupa_zajeciowa sroda[5];
+	Grupa_zajeciowa czwartek[5];
+	Grupa_zajeciowa piatek[5];
+
+
+
+
+	Harmonogram(string, Trener);
+
+	Harmonogram stworz_harmonogram(Grupa_zajeciowa, Harmonogram );
+
+	Harmonogram modyfikuj_harmonogram(Grupa_zajeciowa, Grupa_zajeciowa, Harmonogram);
+
+	Harmonogram usun_zajecia(Grupa_zajeciowa, Harmonogram);
+
+};
+#endif
+
