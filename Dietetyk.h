d00de9d868c230510e0d75d0785cf3b393a61d6e
Ejdzrian <60348246+Ejdzrian@users.noreply.github.com>
2021-01-19 19:11:41 +0100

    Update Dietetyk.h

--- Dietetyk.h
+++ Dietetyk.h
@@ -21,6 +21,16 @@ class Dietetyk
 
 public: 
 	std::vector<Wizyta_u_dietetyka*> _unnamed_Wizyta_u_dietetyka_;
+	
+	std::vector <Wizyta_u_dietetyka*> _plan_wizyt_pon;
+	
+	std::vector <Wizyta_u_dietetyka*> _plan_wizyt_wt;
+	
+	std::vector <Wizyta_u_dietetyka*> _plan_wizyt_sr;
+	
+	std::vector <Wizyta_u_dietetyka*> _plan_wizyt_czw;
+	
+	std::vector <Wizyta_u_dietetyka*> _plan_wizyt_pt;
 
 	Dietetyk(string, string, int);
 
@@ -30,7 +40,11 @@ public:
 
 	Karta_zdrowia* wglad_do_karty_zdrowia(Klient);
 
-	void dodaj_wizyte(Wizyta_u_dietetyka*);
+	Dietetyk dodaj_wizyte(Wizyta_u_dietetyka*);
+
+	Dietetyk dodaj_do_planu_wizyt_dietetyka(Wizyta_u_dietetyka*, Dietetyk);
+
+	Dietetyk wyswietl_plan_tygodnia_dietetyka(Dietetyk);
 
 	
 
