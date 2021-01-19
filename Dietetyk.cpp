8ec61ff06345451fec35f6dbf5b503f4eb3807fa
Ejdzrian <60348246+Ejdzrian@users.noreply.github.com>
2021-01-19 19:14:18 +0100

    Update Dietetyk.cpp

--- Dietetyk.cpp
+++ Dietetyk.cpp
@@ -1,6 +1,7 @@
 #include <exception>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 #include "Dietetyk.h"
@@ -40,4 +41,112 @@ Karta_zdrowia* Dietetyk::wglad_do_karty_zdrowia(Klient k) {
 void Dietetyk::dodaj_wizyte(Wizyta_u_dietetyka* wizyta) {
 	_unnamed_Wizyta_u_dietetyka_.push_back(wizyta);
 }
+
+Dietetyk Dietetyk:: dodaj_do_planu_wizyt_dietetyka(Wizyta_u_dietetyka* wizyta, Dietetyk a) {
+		if (wizyta->get_dzien == 1)
+		{
+			for (int i = 0; i < a._plan_wizyt_pon.size(); i++)
+			{
+				if (a._plan_wizyt_pon[i]->get_godzina != wizyta->get_godzina)
+				{
+					a._plan_wizyt_pon.push_back(wizyta);
+					cout << "Wizyta zapisana poprawnie!" << endl;
+				}
+			}
+			sort(a._plan_wizyt_pon.begin(),a._plan_wizyt_pon.end());
+			
+			
+		}
+		if (wizyta->get_dzien == 2)
+		{
+			for (int i = 0; i < a._plan_wizyt_wt.size(); i++)
+			{
+				if (a._plan_wizyt_wt[i]->get_godzina != wizyta->get_godzina)
+				{
+					a._plan_wizyt_wt.push_back(wizyta);
+					cout << "Wizyta zapisana poprawnie!" << endl;
+				}
+				
+			}
+			sort(a._plan_wizyt_wt.begin(), a._plan_wizyt_wt.end());
+
+		}
+		if (wizyta->get_dzien == 3)
+		{
+			for (int i = 0; i < a._plan_wizyt_sr.size(); i++)
+			{
+				if (a._plan_wizyt_sr[i]->get_godzina != wizyta->get_godzina)
+				{
+					a._plan_wizyt_sr.push_back(wizyta);
+					cout << "Wizyta zapisana poprawnie!" << endl;
+				}
+				
+			}
+			sort(a._plan_wizyt_sr.begin(), a._plan_wizyt_sr.end());
+
+		}
+		if (wizyta->get_dzien == 4)
+		{
+			for (int i = 0; i < a._plan_wizyt_czw.size(); i++)
+			{
+				if (a._plan_wizyt_czw[i]->get_godzina != wizyta->get_godzina)
+				{
+					a._plan_wizyt_czw.push_back(wizyta);
+					cout << "Wizyta zapisana poprawnie!" << endl;
+				}
+				
+			}
+			sort(a._plan_wizyt_czw.begin(), a._plan_wizyt_czw.end());
+
+		}
+		if (wizyta->get_dzien == 5)
+		{
+			for (int i = 0; i < a._plan_wizyt_pt.size(); i++)
+			{
+				if (a._plan_wizyt_pt[i]->get_godzina != wizyta->get_godzina)
+				{
+					a._plan_wizyt_pt.push_back(wizyta);
+					cout << "Wizyta zapisana poprawnie!" << endl;
+
+				}
+				
+			}
+			sort(a._plan_wizyt_pt.begin(), a._plan_wizyt_pt.end());
+
+		}
+
+		return a;
+
+		
+};
+
+Dietetyk Dietetyk:: wyswietl_plan_tygodnia_dietetyka(Dietetyk a) {
+	   cout << "Poniedziałek: " << endl;
+		for (int i = 0; i < a._plan_wizyt_pon.size(); i++)
+		{
+			cout << "Godzina: " << a._plan_wizyt_pon[i]->get_godzina << "Imie: " << a._plan_wizyt_pon[i]->_unnamed_Klient_->get_imie<< endl;
+		}
+		cout << "Wtorek: " << endl;
+		for (int i = 0; i < a._plan_wizyt_wt.size(); i++)
+		{
+			cout << "Godzina: " << a._plan_wizyt_wt[i]->get_godzina << "Imie: " << a._plan_wizyt_wt[i]->_unnamed_Klient_->get_imie << endl;
+		}
+		cout << "Sroda: " << endl;
+		for (int i = 0; i < a._plan_wizyt_sr.size(); i++)
+		{
+			cout << "Godzina: " << a._plan_wizyt_sr[i]->get_godzina << "Imie: " << a._plan_wizyt_sr[i]->_unnamed_Klient_->get_imie << endl;
+		}
+		cout << "Czwartek: " << endl;
+		for (int i = 0; i < a._plan_wizyt_czw.size(); i++)
+		{
+			cout << "Godzina: " << a._plan_wizyt_czw[i]->get_godzina << "Imie: " << a._plan_wizyt_czw[i]->_unnamed_Klient_->get_imie << endl;
+		}
+		cout << "Piatek: " << endl;
+		for (int i = 0; i < a._plan_wizyt_pt.size(); i++)
+		{
+			cout << "Godzina: " << a._plan_wizyt_pt[i]->get_godzina << "Imie: " << a._plan_wizyt_pt[i]->_unnamed_Klient_->get_imie << endl;
+		}
+
+
+};
 
