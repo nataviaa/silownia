443452c739717633c7c4cf296167c8dbbe1da30d
Klaudia19 <77021636+Klaudia19@users.noreply.github.com>
2021-01-20 20:28:53 +0100

    Update Klient.cpp

--- Klient.cpp
+++ Klient.cpp
@@ -14,7 +14,8 @@ using namespace std;
 #include "Trening_personalny.h"
 #include "Harmonogram.h"
 
-Klient::Klient(string imie , string nazwisko, int rok) {
+Klient::Klient(string imie , string nazwisko, int rok) 
+{
 	_imie = imie;
 	_nazwisko = nazwisko;
 	_rok_urodzenia = rok;
@@ -25,20 +26,23 @@ Klient::Klient(string imie , string nazwisko, int rok) {
 	}
 	cout << "Haslo zostalo automatycznie wygenerowane: " << _haslo << endl;
 }
-void Klient::zapisz_do_grupy(Grupa_zajeciowa* grupa, Klient klient) {
+void Klient::zapisz_do_grupy(Grupa_zajeciowa* grupa, Klient klient) //metoda dodajaca Klienta do grupy zajeciowej
+{
 	//_unnamed_Grupa_zajeciowa.push_back(grupa);
 	_unnamed_Grupa_zajeciowa = grupa;
 	cout << "Klient " << _imie << " jest uczestnikiem zajec w grupie." << endl;
 	grupa->dodaj_uczestnika(klient);
 }
-void Klient::zapisz_do_dietetyka(int dzien, double godzina, int cena, Dietetyk* dietetyk, Klient* klient) {
+void Klient::zapisz_do_dietetyka(int dzien, double godzina, int cena, Dietetyk* dietetyk, Klient* klient) //metoda zapisujaca Klienta na wizyte do dietetyka
+{
 	Wizyta_u_dietetyka wizyta(dzien, godzina, cena, dietetyk, klient);//tworzymy wizyte
 	dietetyk->dodaj_wizyte(&wizyta);//dodajemy wizyte do dietetyka
 	_unnamed_Wizyta_u_dietetyka_.push_back(&wizyta);//dodajemy wizyte do klienta
 	dietetyk->dodaj_do_planu_wizyt_dietetyka(&wizyta, *dietetyk);//dodajemy wizyte do grafiku dietetyka
 	cout << "Klient " << _imie << "zostal zapisany na wizyte do dietetyka." << endl;
 }
-void Klient::zapisz_na_trening(int dzien, double godzina, string rodzaj, Trener* trener, Klient* klient) {
+void Klient::zapisz_na_trening(int dzien, double godzina, string rodzaj, Trener* trener, Klient* klient) //metoda zapisujaca Klienta na trening personalny do trenera
+{
 	Trening_personalny trening(dzien, godzina, rodzaj, trener, klient);
 	trener->dodaj_trening(&trening);
 	_unnamed_Trening_personalny_.push_back(&trening);
@@ -46,7 +50,8 @@ void Klient::zapisz_na_trening(int dzien, double godzina, string rodzaj, Trener*
 	cout << "Klient " << _imie << "zostal zapisany na trening personalny" << endl;
 
 }
-void Klient::uaktualnij_dane(string imie, string nazwisko, string imie_rodzica, string nazwisko_rodzica) {
+void Klient::uaktualnij_dane(string imie, string nazwisko, string imie_rodzica, string nazwisko_rodzica) //metoda, ktora uaktualnia dane Klienta
+{
 	_imie = imie;
 	_nazwisko = nazwisko;
 	_imie_rodzica = imie_rodzica;
@@ -54,7 +59,8 @@ void Klient::uaktualnij_dane(string imie, string nazwisko, string imie_rodzica, 
 
 	cout << "Zamiany w danych klienta zostały zapisane pomyslnie." << endl;
 }
-void Klient::wglad_w_karte_zdrowia() {
+void Klient::wglad_w_karte_zdrowia()
+{
 	if (_unnamed_Karta_zdrowia_ != nullptr)
 	{
 		cout << "Dane w karcie zdrowia: " << endl;
@@ -65,10 +71,13 @@ void Klient::wglad_w_karte_zdrowia() {
 	else
 		cout << "Brak podpietej karty zdrowia." << endl;
 }
-void Klient::dodaj_karte_zdrowia(Karta_zdrowia k ) {
+void Klient::dodaj_karte_zdrowia(Karta_zdrowia k ) //metoda ktora dodaje karte zdrowia do Klienta
+{
 	_unnamed_Karta_zdrowia_ = &k;
 	cout << "Karta zdrowia zostala dodana." << endl;
 }
+
+					
 string Klient::get_imie() {
 	return _imie;
 }
